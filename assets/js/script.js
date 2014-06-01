597a6bef060c44c06afb69c079b076b21e6d5c5b
ninjadoge24 <ninjadoge24@gmail.com>
2014-06-01 21:42:42 +0530

    Refactor main script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
-Posts = []
-
+$("article").html("<small>Fetching posts...</small>");
+var posts = [];
 for (var i = Index.length - 1; i >= 0; i--) {
 	(function(i) {
 		var post = Index[i];
 		$.get("posts/" + post, function(data) {
-			Posts[i] = data;
+			posts[i] = data;
 			$("article").html("");
-			for (var j = Posts.length - 1; j >= 0; j--) {
-				var html = "<section><pre>" + Posts[j] + "</pre></section>";
-				$("article").append(html);
+			for (var j = posts.length - 1; j >= 0; j--) {
+				if (posts[j]) {
+					$("article").append("<section id='" + j + "'></section>");
+					$("#" + j).html("<pre>" + posts[j] + "</pre>");
+				};
 			};
 		});
 	})(i);
