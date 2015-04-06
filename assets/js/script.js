653f47411e42961bdde1ee064c2c1305a74ea514
ninjadoge24 <ninjadoge24@gmail.com>
2015-04-06 11:14:49 +0530

    New markdown parser

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     Selected = window.location.hash.substr(1);
 
     for (var i = Index.length - 1; i >= 0; i--) {
-        $("article").append("<section id='" + Index[i] + "'></section>");
+        $("article").append("<section class='markdown-body' id='" + Index[i] + "'></section>");
     };
 
     window.onhashchange = function() {
@@ -36,9 +36,10 @@ function fetch(callback) {
             var slug = Index[i] + ".md";
             $.get("posts/" + slug, function(content) {
                 Posts[i] = content;
-                Posts[i] += "\n[permalink] (#" + Index[i] + ")";
-                Posts[i] += "\n[history] (https://github.com/ninjadoge24/ninjadoge24.github.io/commits/master/posts/" + slug + ")";
-                Posts[i] += "\n[edit] (https://github.com/ninjadoge24/ninjadoge24.github.io/edit/master/posts/" + slug + ")";
+                Posts[i] += "\n[permalink](#" + Index[i] + ")";
+                Posts[i] += "\n[history](https://github.com/ninjadoge24/ninjadoge24.github.io/commits/master/posts/" + slug + ")";
+                Posts[i] += "\n[edit](https://github.com/ninjadoge24/ninjadoge24.github.io/edit/master/posts/" + slug + ")";
+                console.log(Posts[i]);
                 callback();
             });
         })(i);
@@ -55,7 +56,7 @@ function render() {
         var i = Index.indexOf(Selected);
         var id = "#" + Selected;
         $(".info, section").html("");
-        $(id).html(markdown.toHTML(Posts[i]));
+        $(id).html(marked(Posts[i]));
         var title = $(id).children().first().html();
         var link = "<a href='" + id + "'>" + title + "</a>";
         document.title = title + " - ninjadoge24's blog";
@@ -66,7 +67,7 @@ function render() {
         for (var i = Posts.length - 1; i >= 0; i--) {
             var id = "#" + Index[i];
             if ($(id).html() == "") {
-                $(id).html(markdown.toHTML(Posts[i]));
+                $(id).html(marked(Posts[i]));
                 var title = $(id).children().first().html();
                 var link = "<a href='" + id + "'>" + title + "</a>";
                 $(id).children().first().html(link);
